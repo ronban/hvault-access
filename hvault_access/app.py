6c69a5b7d9aef78903f15f825ab1c915a318213a
ronban <ronban@gmail.com>
2020-05-31 10:52:57 -0500

    Added template.yaml

--- hvault_access/app.py
+++ hvault_access/app.py
@@ -22,41 +22,45 @@ def aws_session(role_arn=None, session_name='my_session'):
 
 
 def lambda_handler(event, context):
-    VAULT_ADDR = os.environ["VAULT_ADDR"]
-    VAULT_HEADER_VALUE = os.environ["VAULT_HEADER_VALUE"]
-    ROLE_ARN = os.environ['ROLE_ARN']
-    SECRET_PATH = os.environ['SECRET_PATH']
-    AWS_REGION = os.environ['C_AWS_REGION']
+    vault_addr = os.environ["VAULT_ADDR"]
+    vault_region = os.environ['VAULT_AWS_REGION']
+    vault_namespace = os.getenv('VAULT_NAMESPACE')
+    vault_bucket = os.getenv('VAULT_BUCKET')
+    vault_cert_loc = os.getenv('VAULT_SERVER_CERT')
+    vault_role = os.getenv('VAULT_ROLE_ARN')
 
-    path = event["queryStringParameters"]['path'] or SECRET_PATH
+    path = event["queryStringParameters"]['path']
+    role = event["queryStringParameters"]['role']
 
-    # session = aws_session(role_arn=ROLE_ARN, session_name='my_vault_lambda')
-    # session_regular = aws_session()
+    #session_assumed = aws_session(role_arn=vault_role, session_name='my_vault_lambda')
+    session_regular = aws_session()
 
-    client = hvac.Client(url=VAULT_ADDR,
-                         verify=False)
+    client = hvac.Client(url=vault_addr,
+                         namespace=vault_namespace,
+                         verify=False
+                         )
 
-    session = boto3.Session()
-    creds = session.get_credentials().get_frozen_credentials()
-    client.auth_aws_iam(
-        creds.access_key,
-        creds.secret_key,
-        creds.token,
-        region=AWS_REGION,
-        role=ROLE_ARN,
-        header_value=VAULT_HEADER_VALUE,
-        use_token=True
+    creds = session_regular.get_credentials().get_frozen_credentials()
+
+    client.auth.aws.iam_login(
+        access_key=creds.access_key,
+        secret_key=creds.secret_key,
+        session_token=creds.token,
+        role=role,
+        use_token=True,
+        region=vault_region
     )
 
     secret_version_response = client.secrets.kv.v2.read_secret_version(
         mount_point='kv',
-        path=path,
+        path=path
     )
 
     secret_dict = secret_version_response['data']['data']
 
     date = secret_version_response['data']['metadata']['created_time']
     ver = secret_version_response['data']['metadata']['version']
+
     value = ','.join(secret_dict.values())
 
     return {
